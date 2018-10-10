0b43572a8e2d1738a23c15a7608c81288aad7161
Nuno Teixeira <nunowallenstein@gmail.com>
2018-10-10 20:41:58 +0100

    Introdução de variáveis

--- Trabalho_2/Serie2.cpp
+++ Trabalho_2/Serie2.cpp
@@ -6,7 +6,8 @@ static const int N=1;
 
 static const int N_iter=10;
 
-static const double dt =0.01;
+static const double dt =1;
+
 
 int main(int argc, char *argv[])
 {
@@ -15,19 +16,52 @@ int main(int argc, char *argv[])
       cout << "This program works with two arguments, execute it by the following line" << endl << "./thisfile [length] [N_sheets]" << endl;
       return 0;
     }
-  double length=atol(argv[1]);
-    
+  //Aquisição dos argumentos, comprimento da caixa e o número de folhas 
+  double length=atol(argv[1]);    
   int N_sheets=atoi(argv[2]);
    
+  //Posições iniciais
+  double x_i=1;
+  double v_i=0;
+
+  
     
   /*
   cout << "teste" << endl;
   cout	<< "Nsheet=" << N_sheets<< endl;
   cout	<<"length="<<length<<endl ;
   */
-  double x_eq[N_sheets];
-  //depois pomos o método de euler para as velocidades e posições e tá a andar
-    
+  double *x_eq;
+  double *x;
+  double *v;
 
+  
+  double *Dlt_x;
+  double *Dlt_v;
+
+  
+  x_eq=new double[N_sheets];
+  x=new double[N_sheets];
+  v=new double[N_sheets];
+  Dlt_x=new double[N_sheets];
+  Dlt_v=new double[N_sheets];
+  for (int i=0;i<N_sheets;i++)
+    {
+      x_eq[i]=(length/(2*N_sheets))+ (i * length/N_sheets);
+      cout << x_eq[i] << endl;
+      x[i]=x_i;
+      v[i]=v_i;
+    }
+
+
+  for(int i=0;i<N_iter;i++)
+    {
+
+    }
+
+  
+  delete[] x_eq;
+  //delete[] x_i;
+  // delete[] v_i;
   return 0;   
 }
