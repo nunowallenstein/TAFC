ea25da4ec8018470eb271c81cbad14707664f659
Nuno Teixeira <nteixeira@localhost.localdomain>
2018-10-10 19:00:03 +0100

    intro S2 v2

--- Trabalho_2/Serie2.cpp
+++ Trabalho_2/Serie2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 static const int N=1;
@@ -7,12 +8,26 @@ static const int N_iter=10;
 
 static const double dt =0.01;
 
-int main()
-  {
-    // int x=1;
-    // cout << "teste" << endl << "x=" << x << endl;
+int main(int argc, char *argv[])
+{
+  if (argc!=3)
+    {
+      cout << "This program works with two arguments, execute it by the following line" << endl << "./thisfile [length] [N_sheets]" << endl;
+      return 0;
+    }
+  double length=atol(argv[1]);
+    
+  int N_sheets=atoi(argv[2]);
+   
+    
+  /*
+  cout << "teste" << endl;
+  cout	<< "Nsheet=" << N_sheets<< endl;
+  cout	<<"length="<<length<<endl ;
+  */
+  double x_eq[N_sheets];
+  //depois pomos o método de euler para as velocidades e posições e tá a andar
+    
 
-    double x;
-
-    return 0;   
+  return 0;   
 }
